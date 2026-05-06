d0685c8f0b53abfce174aed2e025a5ec29f811bd
pevacik <nakavalnia@gmail.com>
2026-05-06 23:14:45 +0500

    Счетчики

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from "react"
+import Counter from "./components/counter"
 
 function App() {
 
-  const [likes, setLikes] = useState(5)
+  
   const [value, setValue] = useState('текст в инпуте')
 
-function increment(){
-setLikes(likes + 1)
-}
 
-function decrement (){
-setLikes(likes - 1)
-}
 
 function changeInput (){
 senValue()
@@ -20,24 +15,7 @@ senValue()
 
   return (
     <div>
-
-
-    круто приложение
-
-    <h1>{likes}</h1>
-    <h1>{value}</h1>
-
-    <input type="text" value={value} 
-    onChange={(e)=>{setValue(e.target.value)}}
-    />
-    <button
-    onClick={increment}
-    >increment</button>
-
-    <button
-    onClick={decrement}    
-    >decrement</button>
-  
+      <Counter/>
     </div>
   )
 }
