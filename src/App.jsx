e489084387a3b19aac0fa365893805528f2d4030
pevacik <nakavalnia@gmail.com>
2026-05-06 23:46:40 +0500

    Классовый подход

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react"
-import Counter from "./components/counter"
+import React, { useState } from 'react'
+import Counter from './components/counter'
+import ClassCounter from './components/ClassCounter'
 
 function App() {
-
-  
   const [value, setValue] = useState('текст в инпуте')
 
-
-
-function changeInput (){
-senValue()
-
-}
+  function changeInput() {
+    senValue()
+  }
 
   return (
     <div>
-      <Counter/>
+      <Counter />
+      <ClassCounter />
+      
     </div>
   )
 }
