126cf300d3c5efe8bfb1f65145acacbbdc1e12a8
pevacik <nakavalnia@gmail.com>
2026-05-07 23:31:10 +0500

    начало UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,23 @@ import ClassCounter from './components/ClassCounter'
 import './components/styles/App.css'
 import Postitem from './components/Postitem'
 import { useState } from 'react'
+import PostList from './components/PostList'
 
 function App() {
   const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'Javascript',
-      body: 'Discription',
-    },
-    {
-      id: 2,
-      title: 'Javascript',
-      body: 'Discription',
-    },
-    {
-      id: 3,
-      title: 'Javascript',
-      body: 'Discription',
-    },
+    { id: 1, title: 'Javascript', body: 'Discription' },
+    { id: 2, title: 'Javascript', body: 'Discription' },
+    { id: 3, title: 'Javascript', body: 'Discription' },
   ])
 
   return (
     <div className='App'>
-      <Counter />
-      <ClassCounter />
-      <Postitem
-        post={{
-          id: 1,
-          title: 'Javascript',
-          body: 'Discription',
-        }}
-
-      
-      />
+      <form>
+        <input type='text' placeholder='Название поста' />
+        <input type='text' placeholder='Описание поста' />
+        <button>Создать пост</button>
+      </form>
+      <PostList posts={posts} title='Список постов 1' />
     </div>
   )
 }
--- src/components/Postitem.jsx
+++ src/components/Postitem.jsx
@@ -3,7 +3,9 @@ const Postitem = (props) => {
     <div>
       <div className='post'>
         <div className='post__content'>
-          <strong>{props.post.id}.{props.post.title}</strong>
+          <strong>
+            {props.post.id}.{props.post.title}
+          </strong>
           <div>{props.post.body}</div>
         </div>
         <div className='post__btns'>
