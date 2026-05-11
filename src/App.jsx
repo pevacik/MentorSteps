f72cd2d30eb3e0403bf3c1d244149c44a427d093
pevacik <nakavalnia@gmail.com>
2026-05-12 02:03:48 +0500

    транзишен, аксиос, пагинация,

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import './components/styles/App.css'
 import PostList from './components/PostList'
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 import PostForm from './components/PostForm'
 import PostFilter from './components/PostFilter'
 import MyModal from './components/UI/modal/MyModal'
+import Mybutton from './components/UI/button/Mybutton'
+import { usePosts } from './components/hooks/usePosts'
+import axios from 'axios'
+import PostService from './components/API/PostService'
+import Loader from './components/UI/loader/Loader'
+import { useFatching } from './components/hooks/useFatching'
+import { getPageCount, getPagesArray } from './components/utils/pages'
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Discription' },
-    { id: 2, title: 'Javascript', body: 'Discription' },
-    { id: 3, title: 'Javascript', body: 'Discription' },
-  ])
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({ sort: '', query: '',})
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-  const [filter, setFilter] = useState({
-    sort: '',
-    query: '',
+  const [totalPage, setTotalPage] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  const [fetchPosts, isPostsLoading, postError]= useFatching(async ()=> {
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPage(getPageCount(totalCount, limit))
   })
 
-  const sortedPosts = useMemo(() => {
-    console.log('функция отработала')
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort]),
-      )
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLocaleLowerCase().includes(filter.query),
-    )
-  }, [filter.query, sortedPosts])
-
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  let pageArray = getPagesArray(totalPage)
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -40,11 +40,23 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
-
+  const changePage =()=>{
+    setPage(page)
+    
+  }
   return (
 
     <div className='App'>
-      <MyModal visible={true} >
+      <button onClick={fetchPosts} >Get Posts</button>
+      <Mybutton
+        style={{marginTop: 30}}
+        onClick={()=>{setModal(true)}}
+      >
+      Создать пользователя
+      </Mybutton>
+
+      <MyModal visible={modal}
+        setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
 
@@ -53,12 +65,31 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
+      {postError &&
+      <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading
+        ?<div style={{display:'flex', justifyContent: 'center', marginTop: 50}} >
+          <Loader/>
+        </div>
 
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title='Список постов 1'
-      />
+        : <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title='Список постов 1'
+        />
+      }
+
+      <div className='page__wrapper' >
+        {pageArray.map(p=>
+          <span
+            onClick={()=>changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'} >
+            {p}
+          </span>
+        )}
+      </div>
 
     </div>
   )
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { createRef } from 'react' // 1. Добавь createRef
 import Postitem from './Postitem'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 const PostList = ({ posts, title, remove }) => {
 
   if(!posts.length){
-    return (
-      <h1 style={{ textAlign: 'center' }}>
-        Посты не найдеты!
-      </h1>
-    )
+    return <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
   }
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map((post, index) => (
-        <Postitem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id}
-        />
-      ))}
+      <TransitionGroup>
+        {posts.map((post, index) => {
+
+          const nodeRef = createRef(null)
+
+          return (
+            <CSSTransition
+              key={post.id}
+              nodeRef={nodeRef}
+              timeout={500}
+              classNames="post"
+            >
+
+              <div ref={nodeRef}>
+                <Postitem
+                  remove={remove}
+                  number={index + 1}
+                  post={post}
+                />
+              </div>
+            </CSSTransition>
+          )
+        })}
+      </TransitionGroup>
     </div>
   )
 }
